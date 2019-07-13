6560ddef514450581b6a466e2adae7c22508a393
Bad Joke <32912199+Badly-Timed-Joke@users.noreply.github.com>
2019-07-12 22:24:00 -0400

    Previous commit couldn't build
    Created a new project instead of importing directly from Github

--- app/src/main/java/com/example/innovatorsetup/MainActivity.java
+++ app/src/main/java/com/example/innovatorsetup/MainActivity.java
@@ -1,20 +1,23 @@
-package com.example.setupmodule;
+package com.example.innovatorsetup;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SetupActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,7 +34,7 @@ public class SetupActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_setup, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
