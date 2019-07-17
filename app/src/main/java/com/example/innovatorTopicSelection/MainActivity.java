2821c9aa2a572273573250a66244da845c402834
TomGao2001 <gzm2001@outlook.com>
2019-07-16 21:14:39 -0400

    Testing custom progressbar
    app on emulator quits on start

--- app/src/main/java/com/example/innovatorTopicSelection/MainActivity.java
+++ app/src/main/java/com/example/innovatorTopicSelection/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.innovatorTopicSelection;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.innovatorsetup.R;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Fragment {
 
     Button startPracticeButton;
+    ImageButton leftButton, rightButton;
+    ImageView left, current, right;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_topic_select, container, false);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_topic_select);
-        startPracticeButton = findViewById(R.id.practice_start_button);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        startPracticeButton = (Button) getView().findViewById(R.id.practice_start_button);
+        //leftButton = (ImageButton) getView().findViewById(R.id.)
+        //left = getView().findViewById();
+        //current = (ImageView) getView().findViewById();
+        //right = getView().findViewById();
 
+        //this is triggered soon after OCV above.
+        //any view setup here (view lookups, view listener attach)
 
+        //setup any handles to view objects here (?)
 
+        super.onViewCreated(view, savedInstanceState);
     }
 }
---
+++ app/src/main/java/com/example/innovatorTopicSelection/ProgressBarTestActivity.java
@@ -0,0 +1,16 @@
+package com.example.innovatorTopicSelection;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.innovatorsetup.R;
+
+public class ProgressBarTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.progress_bar_test);
+    }
+}
