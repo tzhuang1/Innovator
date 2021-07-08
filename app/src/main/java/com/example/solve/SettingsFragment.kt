d8ef46a9cf4e45143f9be78049dfc051026b948d
Eric Wang <ericw029@gmail.com>
2021-07-08 09:41:44 -0400

    added toast to settings save

--- app/src/main/java/com/example/solve/SettingsFragment.kt
+++ app/src/main/java/com/example/solve/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package com.example.solve
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.settings_fragment.*
 
 
 class SettingsFragment : Fragment() {
@@ -17,7 +20,7 @@ class SettingsFragment : Fragment() {
     private lateinit var viewModel: SettingsViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.settings_fragment, container, false)
@@ -26,7 +29,14 @@ class SettingsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+
+        settingsSaveBtn.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this.requireActivity(), "Settings saved.", Toast.LENGTH_SHORT).show()
+        })
+
         // TODO: Use the ViewModel
     }
+
+
 
 }
