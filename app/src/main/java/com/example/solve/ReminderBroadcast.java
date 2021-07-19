e446249661554d0325a5efe8ce225438b1dd4c59
Eric Wang <ericw029@gmail.com>
2021-07-19 11:16:02 -0400

    added notifs to settings screen, sends notif 10 seconds after save

---
+++ app/src/main/java/com/example/solve/ReminderBroadcast.java
@@ -0,0 +1,26 @@
+package com.example.solve;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class ReminderBroadcast extends BroadcastReceiver {
+    //sets notification
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //Still need to change to different message if goal met
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notif")
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle("SOLve Practice Reminder")
+                .setContentText("Hey! You haven't met your daily practice goal yet today! Keep up the progress!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notifManager = NotificationManagerCompat.from(context);
+
+        notifManager.notify(1, builder.build()); //define ID as any number
+
+    }
+}
--- app/src/main/java/com/example/solve/SettingsFragment.kt
+++ app/src/main/java/com/example/solve/SettingsFragment.kt
@@ -1,15 +1,23 @@
 package com.example.solve
 
+import android.app.*
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.timepicker.TimeFormat
 import kotlinx.android.synthetic.main.settings_fragment.*
+import java.util.*
 
 
 class SettingsFragment : Fragment() {
@@ -19,6 +27,10 @@ class SettingsFragment : Fragment() {
     var gradeStr: String? = null
     var actPerDayStr: String? = null
     var notifsBool: Boolean? = false
+    var notifsTime: String? = null
+
+    var displayHour: Int = 0
+    var displayMinute: Int = 0
 
     companion object {
         fun newInstance() = SettingsFragment()
@@ -37,22 +49,97 @@ class SettingsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
 
+        createNotificationChannel()
+
+        //Load saved data
         saved = this.getActivity()?.getSharedPreferences("pref", Context.MODE_PRIVATE);
         gradeSettingsNumInput.setText(saved?.getString("grade", ""))
         activitiesPerDayNumInput.setText(saved?.getString("activitiesPerDay",""))
-        saved?.getBoolean("notifsChecked", false)?.let { notificationsCheckbox.setChecked(it) }
+        saved?.getBoolean("notifsChecked", false)?.let { notifsCheckbox.setChecked(it) }
+        notifsTimeDisplay.setText(saved?.getString("notifsTime",""))
 
+        if (notifsCheckbox.isChecked()) {
+            notifsTimeTxtView.isEnabled = true
+            notifsTimeDisplay.isEnabled = true
+            notifsTimeBtn.isEnabled = true
+        }
+        else{
+            notifsTimeTxtView.isEnabled = false
+            notifsTimeDisplay.isEnabled = false
+            notifsTimeBtn.isEnabled = false
+        }
+
+        notifsCheckbox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                notifsTimeTxtView.isEnabled = true
+                notifsTimeDisplay.isEnabled = true
+                notifsTimeBtn.isEnabled = true
+            }
+            else{
+                notifsTimeTxtView.isEnabled = false
+                notifsTimeDisplay.isEnabled = false
+                notifsTimeBtn.isEnabled = false
+            }
+        }
+
+        //Set notification time
+        notifsTimeBtn.setOnClickListener(View.OnClickListener {
+            val timePickerDialog = TimePickerDialog(getActivity(), android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                { view, hourOfDay, minute ->
+                    displayHour = hourOfDay
+                    displayMinute = minute
+
+                    val calendar: Calendar = Calendar.getInstance()
+                    calendar.set(0, 0, 0, displayHour, displayMinute)
+
+                    // Display Selected date in textbox
+                    notifsTimeDisplay.setText(DateFormat.format("hh:mm aa", calendar))
+                }, 12, 0, false)
+            timePickerDialog.getWindow()?.setBackgroundDrawableResource(android.R.color.transparent);
+
+            timePickerDialog.updateTime(displayHour, displayMinute)
+            timePickerDialog.show()
+
+        })
+
+        //Save button, uses shared preferences to save data locally
         settingsSaveBtn.setOnClickListener(View.OnClickListener {
             gradeStr = gradeSettingsNumInput.getText().toString()
             actPerDayStr = activitiesPerDayNumInput.getText().toString()
-            notifsBool = notificationsCheckbox.isChecked()
+            notifsBool = notifsCheckbox.isChecked()
+            notifsTime = notifsTimeDisplay.getText().toString()
 
             sp = this.getActivity()?.getSharedPreferences("pref", Context.MODE_PRIVATE);
             val editor = sp!!.edit()
             editor.putString("grade", gradeStr)
             editor.putString("activitiesPerDay", actPerDayStr)
             editor.putBoolean("notifsChecked", notifsBool!!)
+            editor.putString("notifsTime", notifsTime)
             editor.commit()
+
+            //Notifications
+            if (notifsBool as Boolean) {
+
+                val intent = Intent(
+                    this.activity,
+                    ReminderBroadcast::class.java
+                )
+                val pendingIntent = PendingIntent.getBroadcast(this.activity, 0, intent, 0)
+                val alarmManager = context?.let { it1 -> getSystemService(it1, AlarmManager::class.java) }
+                val timeAtButtonClick = System.currentTimeMillis()
+                val tenSecondsInMillis = (1000 * 10).toLong()
+
+                if (alarmManager != null) {
+                    alarmManager.set(
+                        AlarmManager.RTC_WAKEUP,
+                        timeAtButtonClick + tenSecondsInMillis,
+                        pendingIntent
+                    )
+                }
+            }
+            else{
+                //disable the notification system
+            }
 
             Toast.makeText(this.requireActivity(), "Settings saved.", Toast.LENGTH_SHORT).show()
         })
@@ -60,5 +147,16 @@ class SettingsFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
-
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = "ReminderChannel"
+            val description = "Channel for reminder notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("notif", name, importance)
+            channel.description = description
+            val notificationManager: NotificationManager? =
+                context?.let { getSystemService(it, NotificationManager::class.java) }
+            notificationManager!!.createNotificationChannel(channel)
+        }
+    }
 }
