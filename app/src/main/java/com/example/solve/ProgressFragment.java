a2a61939f2e832877b568d979104f896754f057c
Eric Wang <ericw029@gmail.com>
2021-07-22 09:27:18 -0400

    created progress screen layout

--- app/src/main/java/com/example/solve/ProgressFragment.java
+++ app/src/main/java/com/example/solve/ProgressFragment.java
@@ -1,25 +1,10 @@
 package com.example.solve;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class ProgressFragment extends Fragment {
 
