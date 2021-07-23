738353b4ad69c093fdbb19808b09f59a6d329aea
eg337 <68549574+eg337@users.noreply.github.com>
2021-07-23 09:35:21 -0400

    Add files via upload

--- app/src/main/java/com/example/solve/AccountFragment.kt
+++ app/src/main/java/com/example/solve/AccountFragment.kt
@@ -1,6 +1,7 @@
 package com.example.solve
 
 import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -30,6 +31,10 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.account_fragment.*
+import androidx.annotation.NonNull
+import com.example.solve.UserData
+import com.google.android.gms.tasks.OnCompleteListener
+
 
 
 class AccountFragment : GoogleApiClient.ConnectionCallbacks,
