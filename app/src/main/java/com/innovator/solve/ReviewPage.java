1fae7a5467b052223600d287e9640c3c8a0e9598
scienceqiu <scienceqiu@gmail.com>
2023-08-02 11:41:59 -0400

    updated help and merged code for Solve

--- app/src/main/java/com/innovator/solve/ReviewPage.java
+++ app/src/main/java/com/innovator/solve/ReviewPage.java
@@ -6,12 +6,11 @@ import android.widget.TextView;
 
 public class ReviewPage {
     static int i = 0;
-    static int numberquestion = 1;
     static String[] question = {"MATH QUESTION: WHAT IS 1+1!!!!!! OMGGGGG YAYAYAYAYAYAYYAY","What is 2+2"};
     static String[] answer = {"Well, 1+1 is equal to 2. You know why? Well, it is because if Jeff and Bob put 1 cookie each on a table, then in total, there will be 2 cookies. So, 1+1 = 2", "What the hell"};
 
     public void question(TextView x){
-        String a = Integer.toString(numberquestion);
+        String a = Integer.toString(i+1);
         x.setText("Question" + " " + "#" + a + ":" + " " + question[i]);
     }
 
@@ -19,24 +18,27 @@ public class ReviewPage {
         x.setText("Answer" + ":" + " " + answer[i]);
     }
 
-    public void Click (Button x, TextView question, TextView answer){
+    public void Click (Button x, TextView q, TextView answer){
         x.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 i += 1;
-                numberquestion += 1;
-                question(question);
+                i%= question.length;
+
+                question(q);
                 answer(answer);
             }
         });
     }
-    public void back (Button x, TextView question, TextView answer){
+    public void back (Button x, TextView q, TextView answer){
         x.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 i -= 1;
-                numberquestion -= 1;
-                question(question);
+                i+=question.length;
+                i%= question.length;
+                System.out.println("NO WTF");
+                question(q);
                 answer(answer);
             }
         });
